dd3ba0b9935a21319b2de4440c178415a7a75a3c
Natalia Dzera <dzera.nat.i@gmail.com>
2020-11-09 09:41:55 +0300

    add to git repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const fruits = {
+    1: 'Яблоко',
+    2: 'Груша',
+    3: 'Банан',
+    4: 'Киви',
+    5: 'Ананас',
+    6: 'Папайя',
+    7: 'Мандарин',
+    8: 'Апельсин',
+    9: 'Айва',
+    10: 'Лимон',
+  }
+
+  const [result, setResult] = useState('Яблоко');
+
+  const handleChange = (event) => {
+    const id = event.target.value;
+
+    for (const [key, value] of Object.entries(fruits)) {
+      if (key === id) {
+        setResult(value)
+      } 
+    }
+    
+    if (!Object.keys(fruits).includes(id)) {
+      setResult('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form className="form">
+        <div className="result">{result}</div>
+        <input name="fruitId" onChange={handleChange} />
+      </form>
     </div>
   );
 }
